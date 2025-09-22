b6ccef6e2a33499c0675a0c180593d60757fc606
Claudemir Conzatti - Planner Soluçoes Empresariais <claudemir@plannersolucoes.com.br>
2025-09-22 09:49:04 -0300

    contas a pagar

--- resources/views/pagar/listAll.blade.php
+++ resources/views/pagar/listAll.blade.php
@@ -53,30 +53,54 @@
         <thead>
             <tr>
                 <th width="5%">NF</th>
-                <th width="5%">TIPO</th>
-                <th width="26%">VENDEDOR</th>
-                <th width="30%">CLIENTE</th>
+                <th width="5%">PARC.</th>
+                <th width="30%">FORNECEDOR</th>
                 <th width="5%">EMISSÃO</th>
-                <th width="5%">QTD</th>
-                <th width="8%">FAT1</th>
-                <th width="8%">FAT2</th>
-                <th width="8%">FAT.TOTAL</th>
+                <th width="5%">VENCIMENTO</th>
+                <th width="8%">VALOR</th>
+                <th width="5%">JUROS</th>
+                <th width="5%">MULTA</th>
+                <th width="5%">DESCONTO</th>
+                <th width="5%">TOTAL</th>
             </tr>
         </thead>
-        @php
-            $qtd_total = 0;
-            $fat_total1 = 0;
-            $fat_total2 = 0;
-        @endphp
         <tbody>
+            @php
+                $vlr_total          = 0;
+                $vlr_juros_total    = 0;
+                $vlr_multa_total    = 0;
+                $vlr_desconto_total = 0;
+            @endphp
+
+            @foreach ($pagar as $item )
+                @php
+                    $vlr_total          += $item->CON_VALOR_ORIGINAL;
+                    $vlr_juros_total    += $item->CON_VALOR_JUROS;
+                    $vlr_multa_total    += $item->CON_VALOR_MULTA;
+                    $vlr_desconto_total += $item->CON_VALOR_DESCONTO;
+                @endphp
+                <tr>
+                    <td>{{$item->CON_NUMERO }}</td>
+                    <td align="center">{{$item->CON_SEQUENCIA }}</td>
+                    <td>{{$item->PART_NOME }}</td>
+                    <td align="center">{{date('d/m/Y',strtotime($item->CON_DT_INCLUSAO)) }}</td>
+                    <td align="center">{{date('d/m/Y',strtotime($item->CON_DT_VENCIMENTO)) }}</td>
+                    <td align="right">{{number_format($item->CON_VALOR_ORIGINAL,2,',','.') }}</td>
+                    <td align="right">{{number_format($item->CON_VALOR_JUROS,2,',','.') }}</td>
+                    <td align="right">{{number_format($item->CON_VALOR_MULTA,2,',','.') }}</td>
+                    <td align="right">{{number_format($item->CON_VALOR_DESCONTO,2,',','.') }}</td>
+                    <td align="right">{{number_format($item->CON_VALOR_ORIGINAL+$item->CON_VALOR_JUROS+$item->CON_VALOR_MULTA-$item->CON_VALOR_DESCONTO,2,',','.') }}</td>
+                </tr>
+            @endforeach
         </tbody>
         <tfoot>
             <tr>
-                <td colspan="5">TOTAL</td>
-                <td align="right">{{number_format($qtd_total,4,',','.')}}</td>
-                <td align="right">{{number_format($fat_total1,2,',','.')}}</td>
-                <td align="right">{{number_format($fat_total2,2,',','.')}}</td>
-                <td align="right">{{number_format($fat_total1+$fat_total2,2,',','.')}}</td>
+                <td colspan="5">Total</td>
+                <td align="right">{{number_format($vlr_total,2,',','.') }}</td>
+                <td align="right">{{number_format($vlr_juros_total,2,',','.') }}</td>
+                <td align="right">{{number_format($vlr_multa_total,2,',','.') }}</td>
+                <td align="right">{{number_format($vlr_desconto_total,2,',','.') }}</td>
+                <td align="right">{{number_format($vlr_total+$vlr_juros_total+$vlr_multa_total-$vlr_desconto_total,2,',','.') }}</td>
             </tr>
         </tfoot>
     </table>
